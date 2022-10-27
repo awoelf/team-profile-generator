483ecb2fb5210d8a42ced8cf2c25852d84c59a32
awoelf <98348782+awoelf@users.noreply.github.com>
2022-10-26 23:38:03 -0500

    Added employee classes and prompts for adding employees

--- index.js
+++ index.js
@@ -2,58 +2,76 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
+// Initialize the program
 const init = () => {
-    console.log('Welcome to the Team Profile Generator.')
-    memberInfo('manager');
-}
+  console.log('Welcome to the Team Profile Generator.');
+  // The first employee is the manager
+  addEmployee('manager');
+};
 
-const addMember = () => {
-    inquirer.prompt([
-        {
-            type: 'checkbox',
-            message: 'Would you like to add a new team member?',
-            name: 'newMember',
-            choices: ['engineer', 'intern', 'No, finish building my team']
-        },
+// Prompts the user if they would like to add more employees or complete building the team.
+const moreEmployees = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'checkbox',
+        message: 'Would you like to add a new team member?',
+        name: 'newMember',
+        choices: ['engineer', 'intern', 'No, finish building my team'],
+      },
     ])
-        .then((response) => {
-            if (response.newMember[0] === 'No, finish building my team'){
-                // Build document
-                console.log('Building document!');
-            } else {
-                memberInfo(response.newMember)
-            }
-        })
-}
-
-const memberInfo = (memberType) => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: `What is the ${memberType}\'s name?`,
-            name: 'name'
-        },
-        {
-            type: 'input',
-            message: `What is the ${memberType}\'s employee id?`,
-            name: 'id'
-        },
-        {
-            type: 'input',
-            message: `What is the ${memberType}\'s email?`,
-            name: 'email'
-        },
-        {
-            type: 'input',
-            message: `What is the ${memberType}\'s office number?`,
-            name: 'office'
+    .then((response) => {
+      if (response.newMember[0] === 'No, finish building my team') {
+        console.log('Building document!');
+      } else {
+        let item;
+        switch (response.newMember[0]) {
+          case 'No, finish building my team':
+            console.log('Building document!');
+            break;
+          case 'engineer':
+            item = 'github username';
+            break;
+          case 'intern':
+            item = 'school';
+            break;
         }
-    ]).then((response) => {
-        console.log(response);
-        // Add manager's info to manager object
-        addMember();
-    })
-}
+        console.log(`Adding ${response.newMember[0]} to the team!`);
+        addEmployee(response.newMember[0], item);
+      }
+    });
+};
+
+const addEmployee = (employee, item = 'office number') => {
+  console.log(item);
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: `What is the ${employee}\'s name?`,
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: `What is the ${employee}\'s employee id?`,
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: `What is the ${employee}\'s email?`,
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: `What is the ${employee}\'s ${item}?`,
+        name: 'item',
+      },
+    ])
+    .then((response) => {
+      console.log(response);
+      // Create employee obj
+      moreEmployees();
+    });
+};
 
 init();
